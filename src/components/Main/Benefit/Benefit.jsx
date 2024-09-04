7c78375444b4bf03ad3f92562f68fa94f64883d7
Pasha Altapov <2004pablo2004@gmail.com>
2024-09-04 10:53:41 +0300

    2

--- src/components/Main/Benefit/Benefit.jsx
+++ src/components/Main/Benefit/Benefit.jsx
@@ -1,4 +1,8 @@
 import React from 'react';
+import icon1 from '../../../images/icon1.png';
+import icon2 from '../../../images/icon2.png';
+import icon3 from '../../../images/icon3.png';
+import icon4 from '../../../images/icon4.png';
 
 export default function Benefit () {
 
@@ -8,22 +12,38 @@ export default function Benefit () {
         <h2 className='benefit__title'>Наши преимущества</h2>
         <div className='benefit__columns'>
           <div className='benefit__item'>
-            <div className='benefit__icon'></div>
+            <span className='benefit__icon' style={{ width: '50px', height: '50px', padding: '25px'}}>
+              <img className='benefit__img'
+               src={icon1}
+               alt="icon1" />
+            </span>
             <h4 className='benefit__subtitle'>Дизайн</h4>
             <p className='benefit__description'>Индивидуальный дизайн для каждого клиента</p>
           </div>
           <div className='benefit__item'>
-            <div className='benefit__icon'></div>
+            <span className='benefit__icon'>
+              <img className='benefit__img'
+               src={icon2}
+               alt="icon2" />
+            </span>
             <h4 className='benefit__subtitle'>Сроки</h4>
             <p className='benefit__description'>​Планшет будет готов через 7-10 дней после утверждения макета</p>
           </div>
           <div className='benefit__item'>
-            <div className='benefit__icon'></div>
+            <span className='benefit__icon'>
+              <img className='benefit__img'
+               src={icon3}
+               alt="icon3" />
+            </span>
             <h4 className='benefit__subtitle'>Качество</h4>
             <p className='benefit__description'>Самые качественные материалы ради Вашего комфорта​</p>
           </div>
           <div className='benefit__item'>
-            <div className='benefit__icon'></div>
+            <span className='benefit__icon' style={{ width: '70px', height: '70px', padding: '15px'}}>
+              <img className='benefit__img'
+               src={icon4}
+               alt="icon4" />
+            </span>
             <h4 className='benefit__subtitle'>Доставка</h4>
             <p className='benefit__description'>​Бесплатная доставка Почтой России</p>
           </div>
